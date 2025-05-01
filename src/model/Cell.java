a793bef8cc248593fa0cd73221fd63b67a7f8294
gunelmirzoeva <mirzoevagunel20@gmail.com>
2025-05-01 11:15:47 +0400

    Create cell class

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Cell {
+    private final Coordinate coordinate;
+    private CellState state;
+
+    public enum CellState {
+        EMPTY,
+        SHIP,
+        HIT,
+        MISS
+    }
+    public Cell(Coordinate coordinate) {
+        this.coordinate = coordinate;
+        this.state = CellState.EMPTY;
+    }
+
+    public Coordinate getCoordinate() {
+        return coordinate;
+    }
+
+    public CellState getState() {
+        return state;
+    }
+
+    public void setState(CellState state) {
+        this.state = state;
+    }
 }
--- src/model/Coordinate.java
+++ src/model/Coordinate.java
@@ -1,27 +1,43 @@
 package model;
 
+import java.util.Objects;
+
 public class Coordinate {
 
-    private int row;
-    private int column;
+    private final int x;
+    private final int y;
 
-    public Coordinate(int row, int column) {
-        this.row = row;
-        this.column = column;
+    public Coordinate(int x, int y) {
+        if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+            throw new IllegalArgumentException("Coordinate out of bounds");
+        }
+        this.x = x;
+        this.y = y;
     }
 
-//
-//    //  FIX IT !!!!
-//    int x;
-//    int y;
-//    public Coordinate(int x,int y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//    public int getX() {
-//        return x;
-//    }
-//    public int getY() {
-//        return y;
-//    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return x == that.x && y == that.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+    @Override
+    public String toString() {
+        char row = (char) ('A' + x);
+        int col = y + 1;
+        return row + Integer.toString(col);
+    }
 }
